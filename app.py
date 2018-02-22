73d2beb3e36ed427c96c537f16f6926605d39221
Adam Smith <adamcsmith98@gmail.com>
2018-02-22 13:34:38 +0000

    Update for new use cases

--- app.py
+++ app.py
@@ -11,13 +11,25 @@ with open('data.json', 'r') as f:
 
 data = json.loads(json_data)
 
-@app.route('/api/', methods=['GET'])
+@app.route('/api/operation', methods=['GET'])
 def get_operation():
-    drug = request.args.to_dict()['drug']
-    operation = request.args.to_dict()['operation']
+    drug = request.args.to_dict()['value1']
+    operation = request.args.to_dict()['value2']
     for entry in data:
         if entry['Drug'] == drug:
-            return json.dumps({'drug': drug, 'operation': operation, 'advice': entry['Advice']})
+            return json.dumps({'type': 'Drug-Operation', 'type1': 'Current Medication', 'type2': 'Operation', 'value1': drug, 'value2': operation, 'advice': entry['Advice'], 'source': 'Drug operation interaction research'})
+
+@app.route('/api/condition', methods=['GET'])
+def get_condition():
+    drug = request.args.to_dict()['value1']
+    condition = request.args.to_dict()['value2']
+    return json.dumps({'type': 'Drug-Condition', 'type1': 'Current Medication', 'type2': 'Condition', 'value1': drug, 'value2': condition, 'advice': 'TODO', 'source': 'Drug condition interaction research'})
+
+@app.route('/api/interaction', methods=['GET'])
+def get_interaction():
+    drug1 = request.args.to_dict()['value1']
+    drug2 = request.args.to_dict()['value2']
+    return json.dumps({'type': 'Drug Interaction', 'type1': 'Current Medication', 'type2': 'New Medication', 'value1': drug1, 'value2': drug2, 'advice': 'TODO', 'source': 'Drug interaction research'})
 
 if __name__ == '__main__':
     app.run()
